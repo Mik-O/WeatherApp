0e9a1cee0a284e74ff109ecee23b3a80d44685da
mikle <mikle.kontinent@gmail.com>
2025-07-03 18:33:47 +0700

    part 1

--- WeatherApp/WeatherViewController.swift
+++ WeatherApp/WeatherViewController.swift
@@ -20,5 +20,25 @@ class WeatherViewController: UIViewController {
         
     }
 
+   
 
+}
+
+// MARK: - Networking
+extension WeatherViewController {
+    private func getWeatherInNetwork() {
+        let urlString = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
+        guard let url = URL(string: urlString) else { return }
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { (data, responce, error) in
+            if let data, let weather = try? JSONDecoder().decode(WeatherData.self, from: data) {
+                self.temperatureLabel.text = "\(weather.current.temperature2M) °C"
+            } else {
+                print("Fail")
+            }
+            
+        }
+        task.resume()
+        
+    }
 }
